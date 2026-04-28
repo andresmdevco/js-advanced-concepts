9b7310f9f17b6051e32e8c13d7c7d0f57a04f848
Andrés Muñoz <andres2078@gmail.com>
2026-04-28 14:45:55 -0500

    feat: add example of promise hell with nested then

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -11,6 +11,13 @@ export const promiseComponent = (element) => {
         element.innerHTML = hero.name;
     }
 
+    const renderTwoHeros = (hero1, hero2) => {
+        element.innerHTML = `
+            <h3>${hero1.name}</h3>
+            <h3>${hero2.name}</h3>
+        `;
+    }
+
     const renderError = (error) => {
         element.innerHTML = `
             <h1>Error:</h1>
@@ -19,11 +26,21 @@ export const promiseComponent = (element) => {
     }
 
     const id1 = '5d86371f233c9f2425f16916';
+    const id2 = '5d86371f2343e37870b91ef1';
+
+
 
     findHero(id1)
-        .then(renderHero)
+        .then((hero1) => {
+
+            findHero(id2)
+                .then((hero2) => {
+                    renderTwoHeros(hero1, hero2)
+                })
+                .catch(renderError);
+        })
         .catch(renderError); 
-        
+
 }
 
 /**
