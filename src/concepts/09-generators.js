1047120b822c1b2b1ab3a5a9f93d5bf2d3a55ebe
Andrés Muñoz <andres2078@gmail.com>
2026-04-29 23:52:34 -0500

    feat: add generator function example

---
+++ src/concepts/09-generators.js
@@ -0,0 +1,31 @@
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const generatorFunctionsComponent = (element) => {
+
+    const myGenerator = myFirstGeneratorFunction();
+
+    console.log(myGenerator.next());
+    console.log(myGenerator.next());
+    console.log(myGenerator.next());
+    console.log(myGenerator.next());
+    console.log(myGenerator.next());
+    console.log(myGenerator.next());
+    console.log(myGenerator.next());
+}
+
+
+
+function* myFirstGeneratorFunction() {
+
+    yield 'Primer valor';
+    yield 'Segundo valor';
+    yield 'Tercer valor';
+    yield 'Cuarto valor';
+
+
+    return 'Ya no hay valores';
+    yield 'ya no se puede hacer nada';
+}
