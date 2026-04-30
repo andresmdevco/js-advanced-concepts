964bbdf1118a4b63989eb1f2e7d51af12856cbf8
Andrés Muñoz <andres2078@gmail.com>
2026-04-29 23:20:06 -0500

    refactor: add try/catch error handling to async/await

--- src/concepts/06-async-await.js
+++ src/concepts/06-async-await.js
@@ -10,11 +10,15 @@ export const asyncAwaitComponent = async(element) => {
     const id1 = '5d86371f97c29d020f1e1f6d';
     const id2 = '5d86371fd55e2e2a30fe1cc3';
 
-    const hero1 = await findHero(id1);
-    const hero2 = await findHero(id2);
-
-    element.innerHTML = `${hero1.name} / ${hero2.name}`
-
+    try {
+        const hero1 = await findHero(id1);
+        const hero2 = await findHero(id2);
+    
+        element.innerHTML = `${hero1.name} / ${hero2.name}`
+        
+    } catch (error) {
+        element.innerHTML = error;
+    }
 }
 
 
@@ -22,8 +26,7 @@ const findHero = async(id) => {
 
     const hero = heroes.find( hero => hero.id === id);
     if (!hero)
-        throw `hero not found`;
+        throw `Hero not found`;
 
     return hero;
-
 }
