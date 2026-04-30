7fa676763e17411da00d38da04faeccbc2287ea4
Andrés Muñoz <andres2078@gmail.com>
2026-04-29 23:35:19 -0500

    feat: optimize non-sequential promises with Promise.all

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { promiseComponent } from './concepts/03-promises'
 import { promiseRaceComponent } from './concepts/04-promise-race'
 import { asyncComponent } from './concepts/05-async'
 import { asyncAwaitComponent } from './concepts/06-async-await'
+import { asyncAwait2Component } from './concepts/07-async-await'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -32,4 +33,5 @@ const element = document.querySelector('.card');
 // promiseComponent(element);
 // promiseRaceComponent(element);
 // asyncComponent(element);
-asyncAwaitComponent(element);
+// asyncAwaitComponent(element);
+asyncAwait2Component(element);
