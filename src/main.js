758895b5cecad15dca6446fda5724810c331e3e0
Andrés Muñoz <andres2078@gmail.com>
2026-04-29 23:50:09 -0500

    feat: add for await example with async iteration

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import { promiseRaceComponent } from './concepts/04-promise-race'
 import { asyncComponent } from './concepts/05-async'
 import { asyncAwaitComponent } from './concepts/06-async-await'
 import { asyncAwait2Component } from './concepts/07-async-await'
+import { forAwaitComponent } from './concepts/08-for-await'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -34,4 +35,5 @@ const element = document.querySelector('.card');
 // promiseRaceComponent(element);
 // asyncComponent(element);
 // asyncAwaitComponent(element);
-asyncAwait2Component(element);
+// asyncAwait2Component(element);
+forAwaitComponent(element);
