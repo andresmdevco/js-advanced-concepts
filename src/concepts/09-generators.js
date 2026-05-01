50697f278b8082bef6911d77cdab2d631a66145c
Andrés Muñoz <andres2078@gmail.com>
2026-04-30 23:50:20 -0500

    feat: use generator function to create incremental ids on click

--- src/concepts/09-generators.js
+++ src/concepts/09-generators.js
@@ -5,17 +5,30 @@
  */
 export const generatorFunctionsComponent = (element) => {
 
-    const myGenerator = myFirstGeneratorFunction();
+    // const myGenerator = myFirstGeneratorFunction();
+    // console.log(myGenerator.next());
 
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
-    console.log(myGenerator.next());
+    const genId = idGenerator();
+
+    const button = document.createElement('button');
+    button.innerText = 'Click me';
+    element.append(button);
+
+    const renderButton = () => {
+        const {value} = genId.next()
+        button.innerText = `Click ${value}`;
+    }
+
+    button.addEventListener('click', renderButton);
 }
 
+
+function* idGenerator() {
+    let currentId = 0;
+    while(true) {
+        yield ++currentId;
+    }
+}
 
 
 function* myFirstGeneratorFunction() {
